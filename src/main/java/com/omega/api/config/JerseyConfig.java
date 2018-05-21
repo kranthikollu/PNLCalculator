2c10d8b50e39a5e3cd6e9c100e6512f0d38e336f
kranthikollu <39473711+kranthikollu@users.noreply.github.com>
2018-05-20 22:54:01 -0400

    Update JerseyConfig.java

--- src/main/java/com/omega/api/config/JerseyConfig.java
+++ src/main/java/com/omega/api/config/JerseyConfig.java
@@ -1,1 +1,27 @@
 
+package com.omega.api;
+
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.glassfish.jersey.servlet.ServletProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.omega.api.config.JerseyConfig;
+
+@SpringBootApplication(scanBasePackages = {"com.omega.api"})
+public class PnlCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnlCalculatorApplication.class, args);
+	}
+	
+	@Bean
+	public ServletRegistrationBean<ServletContainer> jerseyServlet() {
+		ServletRegistrationBean<ServletContainer> registration = new ServletRegistrationBean<ServletContainer>( new ServletContainer(), "/pnl/*");
+		registration.addInitParameter( ServletProperties.JAXRS_APPLICATION_CLASS, JerseyConfig.class.getName() );
+		return registration;
+	}
+	
+}
